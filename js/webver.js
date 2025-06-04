d6650e372e64aee63b8a1d67ebdaf713c182c50a
Muhammad Fachri A.H <muhammadfachrialhusein.1270@gmail.com>
2025-06-05 01:08:10 +0700

    Delete js/webver.js

--- js/webver.js
+++
@@ -1,5 +0,0 @@
-var websiteVersion = "Muhfhri © 2025 v3.9.7"
-var websiteDate = "Pembaruan Terakhir 10 Mei 2025"
-
-document.getElementById("version").textContent = websiteVersion;
-document.getElementById("lastupt").textContent = websiteDate;
